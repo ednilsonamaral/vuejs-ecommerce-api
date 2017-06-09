9678468bb9d76718302887e5914eb2ba257fb265
Ednilson Amaral <ednilsonamaral.ti@gmail.com>
2017-06-09 14:48:24 -0300

    Model e Rotas de Product

---
+++ models/Product.js
@@ -0,0 +1,17 @@
+const Mongoose = require('mongoose')
+const Schema = Mongoose.Schema
+
+const _schema = {
+		name: { type: String, required: true }
+	, price: { type: Number, required: true }
+	, qtd: { type: Number, min: 0, max: 999 }
+	, status: { type: Boolean, required: true }
+	, created_at: Date
+	, updated_at: Date
+}
+
+const ProductSchema = new Schema(_schema)
+
+const Model = Mongoose.model('Product', ProductSchema);
+
+module.exports = Model
---
+++ routes/product.js
@@ -0,0 +1,156 @@
+const CONSTANTS = require('../helpers/constants')
+const URI = `${CONSTANTS.URI}/product`
+const ProductModel = require(`../models/Product`)
+
+const Moment = require('moment')
+const getCurrentDateWithoutTimezone = Moment().format('YYYY-MM-DDTHH:mm:ss')
+
+module.exports = [
+	// Get all products
+	{
+		method: 'GET',
+		path: URI,
+		handler: (req, reply) => {
+			ProductModel.find((error, data) => {
+				if (error) {
+					reply({
+						error: true,
+						data: error,
+						statusCode: 401,
+						statusText: 'NOK',
+					}).code(401)
+				} else {
+					reply({
+						error: false,
+						data: data,
+						statusCode: 200,
+						statusText: 'OK'
+					}).code(200)
+				}
+			})
+		}
+	},
+
+	// Get product by id
+	{
+		method: 'GET',
+		path: URI + `/{id}`,
+		handler: (req, reply) => {
+			ProductModel.findById(req.params.id, (error, data) => {
+				if (error) {
+					reply({
+						error: true,
+						data: error,
+						statusCode: 401,
+						statusText: 'NOK',
+					}).code(401)
+				} else {
+					reply({
+						error: false,
+						data: data,
+						statusCode: 200,
+						statusText: 'OK'
+					}).code(200)
+				}
+			})
+		}
+	},
+
+	// Create a new product
+	{
+		method: 'POST',
+		path: URI,
+		handler: (request, reply) => {
+			const product = new ProductModel({
+					name: request.payload.name
+				, price: request.payload.price
+				, qtd: request.payload.qtd
+				, status: request.payload.status
+				, created_at: getCurrentDateWithoutTimezone
+			})
+
+			product.save((error, data) => {
+				if (error) {
+					reply({
+						error: true,
+						data: error,
+						statusCode: 401,
+						statusText: 'NOK',
+					}).code(401)
+				} else {
+					reply({
+						error: false,
+						data: data,
+						message: 'Novo produto cadastrado com sucesso!',
+						statusCode: 200,
+						statusText: 'OK'
+					}).code(200)
+				}
+			})
+		}
+	},
+
+	// Update a product by id
+	{
+		method: 'PUT',
+		path: URI + `/{id}`,
+		handler: (request, reply) => {
+			const _id = { _id: request.params.id }
+
+			const product = {
+					name: request.payload.name
+				, price: request.payload.price
+				, qtd: request.payload.qtd
+				, status: request.payload.status
+				, updated_at: getCurrentDateWithoutTimezone
+			}
+
+			ProductModel.update(_id, product, { multi: false }, (error, data) => {
+				if (error) {
+					reply({
+						error: true,
+						data: error,
+						statusCode: 401,
+						statusText: 'NOK',
+					}).code(401)
+				} else {
+					reply({
+						error: false,
+						data: data,
+						message: 'Produto editado com sucesso!',
+						statusCode: 200,
+						statusText: 'OK'
+					}).code(200)
+				}
+			})
+		}
+	},
+
+	// Delete a product by id
+	{
+		method: 'DELETE',
+		path: URI + `/{id}`,
+		handler: (request, reply) => {
+			const _id = { _id: request.params.id }
+
+			ProductModel.remove(_id, (error, data) => {
+				if (error) {
+					reply({
+						error: true,
+						data: error,
+						statusCode: 401,
+						statusText: 'NOK',
+					}).code(401)
+				} else {
+					reply({
+						error: false,
+						data: data,
+						message: 'Produto deletado com sucesso!',
+						statusCode: 200,
+						statusText: 'OK'
+					}).code(200)
+				}
+			})
+		}
+	}
+]
